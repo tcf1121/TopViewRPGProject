28f64e76a42e2829d0da70d3f05d251561e1e197
FeAnd <tcf1268@gmail.com>
2025-06-04 12:29:40 +0900

    [Feat] 몬스터 움직임 구현
    몬스터의 움직임을 구현하였다.

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Monster : MonoBehaviour
 {
@@ -10,8 +11,12 @@ public class Monster : MonoBehaviour
     public int MaxHp;
     public int MoveSpeed;
     public int Exp;
-
+    public Animator Animator;
     private int _currentHp;
+    public GameObject AttackRange;
+    public bool CanAttack;
+    public bool IsAttack;
+    UnityAction IsDie;
 
     void Awake() => Init();
 
@@ -25,16 +30,69 @@ public class Monster : MonoBehaviour
         RoomManager.DieMonster();
     }
 
+    void Update()
+    {
+        if (CanAttack)
+        {
+            if (!IsAttack)
+                Animator.SetTrigger("IsAttack");
+            Animator.SetBool("IsMove", true);
+        }
+        else
+        {
+            Animator.SetBool("IsMove", true);
+            Move();
+        }
+    }
+
     private void Init()
     {
         _currentHp = MaxHp;
+        IsDie += Die;
+    }
+
+    private void Move()
+    {
+        Vector3 LookDirection = new Vector3(GameManager.player.gameObject.transform.position.x,
+        transform.position.y, GameManager.player.gameObject.transform.position.z);
+        transform.LookAt(LookDirection);
+        transform.Translate(Vector3.forward * Time.deltaTime * MoveSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Attack"))
-            Destroy(gameObject);
+        {
+            Animator.SetTrigger("IsHit");
+            int PlayerDamage = other.GetComponentInParent<PlayerStatus>().state.Damage;
+            _currentHp -= PlayerDamage;
+            if (_currentHp < 0)
+                IsDie?.Invoke();
+        }
+    }
 
+    private void SetAttack(int value)
+    {
+        if (value == 1)
+        {
+            AttackRange.tag = "MonsterAttack";
+            IsAttack = true;
+        }
+        else
+        {
+            AttackRange.tag = "Untagged";
+            IsAttack = false;
+        }
+    }
+
+    private void Die()
+    {
+        Animator.SetTrigger("IsDie");
+    }
+
+    private void DestroyMon()
+    {
+        Destroy(gameObject);
     }
 }
 
